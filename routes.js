5e28c5b632a7cde401b332e7c94231c78629b4a6
Will Smith <wismith@davidson.edu>
2020-03-10 21:38:49 -0700

    starting on user authentication

--- routes.js
+++ routes.js
@@ -4,6 +4,33 @@ let { ValidationError } = require('objection');
 
 let router = new Router();
 
+// Passport configuration
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+// Post /login
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+
 // GET /
 router.get('/', async(request, response) => {
   let messages = await Message.query().select('*').orderBy('created_at', 'DESC');
@@ -13,6 +40,11 @@ router.get('/', async(request, response) => {
 
 // POST /messages
 router.post('/messages', async(request, response) => {
+  if (!request.user) {
+    alert('You must log in to post to this page!');
+    response.redirect('/login');
+  }
+
   let messageBody = request.body.body;
   let messageTime = new Date();
   console.log(request.body.mood);
